7da78e6ed46ee5ba9a3370998f5995a9eebdef33
LAZYCAT\user <mylazysundaycat@gmail.com>
2023-11-30 22:58:15 +0900

    feat. 장바구니 페이지 UI 완료 / 파일 업데이트 수정 완료

--- src/home/BodyPanel.java
+++ src/home/BodyPanel.java
@@ -1,7 +1,13 @@
 package home;
 
+import home.library.RoundJTextField;
+import home.library.RoundedButton;
+
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class BodyPanel {
     /**
@@ -143,13 +149,173 @@ class ProductPanel extends JPanel{
 
         //TODO 장바구니, 구매하기 버튼에 addActionListener 추가하기
     }
+}
+
+//3. 판매자 글 작성 페이지
+class WritePostPanel extends JPanel implements ActionListener {
+    ImageIcon img_icon;
+    Image img;
+    JLabel img_label, title_label, price_label;
+    RoundedButton complete_btn;
+    JButton img_btn;
+    JTextField title_txt, price_txt;
+
+    public WritePostPanel() {
+        setLayout(null);
+        setBackground(Color.white);
+
+        //TODO 서버연결
+        //사진선택
+        img_icon = new ImageIcon("src/home/image/icon/chooseimg.png");
+        img = img_icon.getImage();
+        img = img.getScaledInstance(380, 380, Image.SCALE_SMOOTH);
+        img_icon = new ImageIcon(img);
+        img_btn = new JButton(img_icon);
+        img_btn.setBounds(50, 20, 380, 380);
+        img_btn.addActionListener(this);
+        img_btn.setBackground(Color.white);
+        img_btn.setBorder(BorderFactory.createLineBorder(Color.white));
+
+        add(img_btn);
+
+        //상품명 라벨
+        title_label = new JLabel("상품명");
+        title_label.setBounds(40,400,120,80);
+        title_label.setFont(new Font("마루 부리OTF",Font.BOLD,22));
+        title_label.setForeground(new Color(33,39,37));
+        add(title_label);
+        //상품명
+        title_txt = new RoundJTextField(30);
+        title_txt.setBackground(new Color(157,184,177));
+        title_txt.setBounds(160,415,270,50);
+        title_txt.setFont(new Font("마루 부리OTF",Font.PLAIN,22));
+        title_txt.setText("");
+        title_txt.setBorder(new LineBorder(new Color(157,184,177)));
+
+        add(title_txt);
+
+        //가격 라벨
+        title_label = new JLabel("가격");
+        title_label.setBounds(40,470,120,80);
+        title_label.setFont(new Font("마루 부리OTF",Font.BOLD,22));
+        title_label.setForeground(new Color(33,39,37));
+        add(title_label);
+        //가격
+        price_txt = new RoundJTextField(30);
+        price_txt.setBackground(new Color(157,184,177));
+        price_txt.setBounds(160,485,270,50);
+        price_txt.setFont(new Font("마루 부리OTF",Font.PLAIN,22));
+        price_txt.setText("");
+        price_txt.setBorder(new LineBorder(new Color(157,184,177)));
+        add(price_txt);
+
+        //작성 완료
+        complete_btn = new RoundedButton("작성 완료");
+        complete_btn.setBackground(new Color(33,39,37));
+        complete_btn.setForeground(Color.white);
+        complete_btn.setFont(new Font("맑은 고딕", Font.BOLD, 37));
+        complete_btn.setBounds(16,580,450,90);
+
+        add(complete_btn);
+
+        setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if(source == img_btn){
+            JFileChooser chooser = new JFileChooser();
+            int r = chooser.showOpenDialog(this);
+            if(r==JFileChooser.APPROVE_OPTION){
+                String name = chooser.getSelectedFile().getAbsolutePath();
+                //사진 재설정
+                ImageIcon newImgIcon = new ImageIcon(name);
+                Image newImg = newImgIcon.getImage();
+                newImg = newImg.getScaledInstance(380, 380, Image.SCALE_SMOOTH);
+                newImgIcon = new ImageIcon(newImg);
+                img_btn.setIcon(newImgIcon);
+                img_btn.setBounds(50, 20, 380, 380);
+
+
+            }
+        }
+    }
+}
+
+//TODO
+//5. 장바구니 페이지
+class BasketPanel extends JPanel{
+    ImageIcon product_icon;
+    Image product_img;
+    JLabel product_img_label, product_title_label, product_price_label, basket_label;
+    JButton basket_btn, buy_btn;
+    JPanel buy_panel;
+    JCheckBox buy_cb;
+    public BasketPanel(){
+        setLayout(null);
+        setSize(500,700);
+        setBackground(Color.white);
+
+        //장바구니 라벨
+        basket_label = new JLabel("장바구니 목록");
+        basket_label.setFont(new Font("맑은고딕", Font.BOLD, 18));
+        basket_label.setForeground(new Color(153,153,153));
+        basket_label.setBounds(15,25, 150,30);
+        add(basket_label);
+
+        //구매하기 버튼
+        buy_btn = new JButton("구매하기");
+        buy_btn.setBackground(new Color(33,39,37));
+        buy_btn.setForeground(Color.white);
+        buy_btn.setFont(new Font("맑은 고딕", Font.BOLD, 37));
+        buy_btn.setBounds(16,580,450,90);
+        add(buy_btn);
+
+        //TODO 서버 연결시 수정해야 할 부분
+        //While, For문을 사용해서 밑에 상품들을 출력해내야 할 것 같습니다~!
+        //구매 패널
+        buy_panel = new JPanel();
+        buy_panel.setLayout(null);
+        buy_panel.setBackground(new Color(157,184,177));
+        buy_panel.setBounds(0,60,500,200);
+        //상품이미지
+        product_icon = new ImageIcon("src/home/image/product/3.png");
+        product_img = product_icon.getImage();
+        product_img = product_img.getScaledInstance(150,150, Image.SCALE_SMOOTH);
+        product_icon = new ImageIcon(product_img);
+        product_img_label = new JLabel(product_icon);
+        product_img_label.setBounds(40,20,150,150);
+        buy_panel.add(product_img_label);
+        //체크박스
+        buy_cb = new JCheckBox();
+        buy_cb.setBounds(5,5,20,20);
+        buy_panel.add(buy_cb);
+        //상품명
+        product_title_label = new JLabel("갤럭시탭S9 플러스");
+        product_title_label.setFont(new Font("맑은고딕", Font.BOLD, 24));
+        product_title_label.setForeground(new Color(72,84,81));
+        product_title_label.setBounds(210, 25, 250,30);
+        buy_panel.add(product_title_label);
+        //상품가격
+        product_price_label = new JLabel("950,000");
+        product_price_label.setFont(new Font("맑은고딕", Font.BOLD, 34));
+        product_price_label.setForeground(new Color(72,84,81));
+        product_price_label.setBounds(210,50,250,60);
+        buy_panel.add(product_price_label);
+
+        add(buy_panel);
+
+        setVisible(true);
+
+        //TODO 장바구니, 구매하기 버튼에 addActionListener 추가하기
+    }
 
 }
 
-//마이페이지 화면
+
 class MypagePanel extends JPanel {}
 //설정 화면 (회원가입 및 로그인)
 class SettingPanel extends JPanel {}
-//상품주문페이지 화면
-class BuyPanel extends JPanel {}
-//회원가입 페이지
+
